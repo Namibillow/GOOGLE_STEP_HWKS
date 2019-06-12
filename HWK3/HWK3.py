2c542bb63a4afe4b5e5ae490e00200be20429070
Nami Akazawa <akazawan73@gmail.com>
2019-06-12 18:11:48 +0900

    HWK3

--- HWK3/HWK3.py
+++ HWK3/HWK3.py
@@ -1,4 +1,5 @@
 def readNumber(line, index):
+    """ handles number """
     number = 0
     while index < len(line) and line[index].isdigit():
         number = number * 10 + int(line[index])
@@ -13,6 +14,13 @@ def readNumber(line, index):
     token = {'type': 'NUMBER', 'number': number}
     return token, index
 
+def readOpenParenthesis(line, index):
+    token = {'type': '('}
+    return token, index + 1
+
+def readCloseParenthesis(line, index):
+    token = {'type': ')'}
+    return token, index + 1
 
 def readPlus(line, index):
     token = {'type': 'PLUS'}
@@ -30,26 +38,62 @@ def readDivision(line, index):
     token = {'type': 'DIVIDE'}
     return token, index + 1
 
-
 def tokenize(line):
+    operators = {'+': readPlus, '-': readMinus, '/': readDivision, '*': readMultiply,
+    '(': readOpenParenthesis, ')': readCloseParenthesis}
+
     tokens = []
     index = 0
     while index < len(line):
         if line[index].isdigit():
             (token, index) = readNumber(line, index)
-        elif line[index] == '+':
-            (token, index) = readPlus(line, index)
-        elif line[index] == '-':
-            (token, index) = readMinus(line, index)
-        elif line[index] == '/':
-            (token, index) = readDivision(line, index)
-        elif line[index] == '*':
-            (token, index) = readMultiply(line, index)
+        elif line[index] in operators.keys():
+            (token, index) = operators[line[index]](line, index)
         else:
             print('Invalid character found: ' + line[index])
             exit(1)
         tokens.append(token)
     return tokens
+
+def parse(tokens):
+    precedences = {'PLUS': 0, 'MINUS':0, 'MULTIPLY':1,'DIVIDE':1}
+    greater_precedence = lambda o1, o2: precedences[o1] >= precedences[o2]
+
+    last_element = lambda l: l[-1]['type'] if l else None
+
+    # STACKS
+    numbers = []
+    operators = []
+
+    for token in tokens:
+        if token['type'] == 'NUMBER':
+            numbers.append(token)
+        elif token['type'] == '(':
+            operators.append(token)
+        elif token['type'] == ')': # if its ) then do the calculation untill encounter to (
+            top = last_element(operators)
+            while top and top!= '(':
+                apply_op(operators, numbers)
+                top = last_element(operators)
+            operators.pop() # remove (
+        else:
+            top = last_element(operators)
+
+            while top and top not in "(" and greater_precedence(top, token['type']):
+                apply_op(operators,numbers)
+                top = last_element(operators)
+            operators.append(token)
+
+    while last_element(operators):
+        apply_op(operators, numbers)
+
+    return numbers[0]['number']
+
+def apply_op(operators, numbers):
+    operator = operators.pop()
+    right = numbers.pop()
+    left = numbers.pop()
+    numbers.append(evaluate([left, operator, right]))
 
 
 def evaluate(tokens):
@@ -57,9 +101,9 @@ def evaluate(tokens):
     tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
     index = 1
 
-    # Handle * and / first 
+    # Handle * and / first
     while index < len(tokens):
-        number = 0 
+        number = 0
         if tokens[index]['type'] == 'NUMBER':
             if tokens[index - 1]['type'] == 'MULTIPLY':
                 number = tokens[index-2]['number'] * tokens[index]['number']
@@ -69,12 +113,11 @@ def evaluate(tokens):
                 number = tokens[index-2]['number'] / tokens[index]['number']
                 tokens = tokens[:index-2] + [{'type': 'NUMBER', 'number': number}] + tokens[index+1:]
                 index-=2
-            elif tokens[index - 1]['type'] in ['MINUS', "PLUS"]:
-                pass
-            else:
+            elif tokens[index - 1]['type'] not in ['MINUS', "PLUS"]:
                 print('Invalid syntax')
-                exit(1)       
-        index +=1 
+                exit(1)
+
+        index +=1
     index = 1
     while index < len(tokens):
         if tokens[index]['type'] == 'NUMBER':
@@ -86,12 +129,13 @@ def evaluate(tokens):
                 print('Invalid syntax')
                 exit(1)
         index += 1
-    return answer
+    return {'type':'NUMBER','number': answer}
 
 
 def test(line):
     tokens = tokenize(line)
-    actualAnswer = evaluate(tokens)
+    actualAnswer = parse(tokens)
+
     expectedAnswer = eval(line)
     if abs(actualAnswer - expectedAnswer) < 1e-8:
         print("PASS! (%s = %f)" % (line, expectedAnswer))
@@ -117,6 +161,11 @@ def runTest():
     test("5/2.0")
     test("10./5")
     test('1*1*1*2/2')
+    test('(1+2)*4*5')
+    test('((1*2)+3)-4')
+    test('5*4-(4/2+3)+(2*2+(2+2))')
+    test('((1+1))')
+    test('((2+2)*(3*3))')
     print("==== Test finished! ====\n")
 
 
@@ -125,11 +174,12 @@ if __name__ == "__main__":
 
     # decimal cannot be first character. eg: 0.13
     # input must be valid.
-    # parenthesis must be matched 
+    # parenthesis must be matched
 
-    while True:
-        print('> ', end="")
-        line = input()
-        tokens = tokenize(line)
-        answer = evaluate(tokens)
-        print("answer = %f\n" % answer)
+    # while True:
+    #     print('> ', end="")
+    #     line = input()
+    #     tokens = tokenize(line)
+    #     answer = parse(tokens)
+    #     print("answer = %f\n" % answer)
+
