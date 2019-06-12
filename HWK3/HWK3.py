b1a3491bc7fd67b410b9a688f87d74a730ef30e6
Nami Akazawa <akazawan73@gmail.com>
2019-06-12 18:36:00 +0900

    add comments

--- HWK3/HWK3.py
+++ HWK3/HWK3.py
@@ -14,33 +14,41 @@ def readNumber(line, index):
     token = {'type': 'NUMBER', 'number': number}
     return token, index
 
+
 def readOpenParenthesis(line, index):
     token = {'type': '('}
     return token, index + 1
+
 
 def readCloseParenthesis(line, index):
     token = {'type': ')'}
     return token, index + 1
 
+
 def readPlus(line, index):
     token = {'type': 'PLUS'}
     return token, index + 1
+
 
 def readMinus(line, index):
     token = {'type': 'MINUS'}
     return token, index + 1
 
+
 def readMultiply(line, index):
     token = {'type': 'MULTIPLY'}
     return token, index + 1
+
 
 def readDivision(line, index):
     token = {'type': 'DIVIDE'}
     return token, index + 1
 
+
 def tokenize(line):
+    ''' break an expression into tokens'''
     operators = {'+': readPlus, '-': readMinus, '/': readDivision, '*': readMultiply,
-    '(': readOpenParenthesis, ')': readCloseParenthesis}
+                 '(': readOpenParenthesis, ')': readCloseParenthesis}
 
     tokens = []
     index = 0
@@ -55,11 +63,13 @@ def tokenize(line):
         tokens.append(token)
     return tokens
 
-def parse(tokens):
-    precedences = {'PLUS': 0, 'MINUS':0, 'MULTIPLY':1,'DIVIDE':1}
-    greater_precedence = lambda o1, o2: precedences[o1] >= precedences[o2]
 
-    last_element = lambda l: l[-1]['type'] if l else None
+def parse(tokens):
+    precedences = {'PLUS': 0, 'MINUS': 0, 'MULTIPLY': 1, 'DIVIDE': 1}
+
+    def greater_precedence(o1, o2): return precedences[o1] >= precedences[o2]
+
+    def last_element(l): return l[-1]['type'] if l else None
 
     # STACKS
     numbers = []
@@ -70,17 +80,17 @@ def parse(tokens):
             numbers.append(token)
         elif token['type'] == '(':
             operators.append(token)
-        elif token['type'] == ')': # if its ) then do the calculation untill encounter to (
+        elif token['type'] == ')':  # if its ) then do the calculation untill encounter to (
             top = last_element(operators)
-            while top and top!= '(':
+            while top and top != '(':
                 apply_op(operators, numbers)
                 top = last_element(operators)
-            operators.pop() # remove (
+            operators.pop()  # remove (
         else:
             top = last_element(operators)
-
+            # process the old operator before appending new one if it has high precedence
             while top and top not in "(" and greater_precedence(top, token['type']):
-                apply_op(operators,numbers)
+                apply_op(operators, numbers)
                 top = last_element(operators)
             operators.append(token)
 
@@ -89,7 +99,9 @@ def parse(tokens):
 
     return numbers[0]['number']
 
+
 def apply_op(operators, numbers):
+    '''apply the operation and append its answer to the stack'''
     operator = operators.pop()
     right = numbers.pop()
     left = numbers.pop()
@@ -98,7 +110,7 @@ def apply_op(operators, numbers):
 
 def evaluate(tokens):
     answer = 0
-    tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
+    tokens.insert(0, {'type': 'PLUS'})  # Insert a dummy '+' token
     index = 1
 
     # Handle * and / first
@@ -106,18 +118,18 @@ def evaluate(tokens):
         number = 0
         if tokens[index]['type'] == 'NUMBER':
             if tokens[index - 1]['type'] == 'MULTIPLY':
-                number = tokens[index-2]['number'] * tokens[index]['number']
-                tokens = tokens[:index-2] + [{'type': 'NUMBER', 'number': number}] + tokens[index+1:]
-                index-=2
+                number = tokens[index - 2]['number'] * tokens[index]['number']
+                tokens = tokens[:index - 2] + [{'type': 'NUMBER', 'number': number}] + tokens[index + 1:]
+                index -= 2
             elif tokens[index - 1]['type'] == 'DIVIDE':
-                number = tokens[index-2]['number'] / tokens[index]['number']
-                tokens = tokens[:index-2] + [{'type': 'NUMBER', 'number': number}] + tokens[index+1:]
-                index-=2
+                number = tokens[index - 2]['number'] / tokens[index]['number']
+                tokens = tokens[:index - 2] + [{'type': 'NUMBER', 'number': number}] + tokens[index + 1:]
+                index -= 2
             elif tokens[index - 1]['type'] not in ['MINUS', "PLUS"]:
                 print('Invalid syntax')
                 exit(1)
 
-        index +=1
+        index += 1
     index = 1
     while index < len(tokens):
         if tokens[index]['type'] == 'NUMBER':
@@ -129,7 +141,7 @@ def evaluate(tokens):
                 print('Invalid syntax')
                 exit(1)
         index += 1
-    return {'type':'NUMBER','number': answer}
+    return {'type': 'NUMBER', 'number': answer}
 
 
 def test(line):
@@ -182,4 +194,3 @@ if __name__ == "__main__":
     #     tokens = tokenize(line)
     #     answer = parse(tokens)
     #     print("answer = %f\n" % answer)
-
