3dc18e3c936137b463d5096afee5baac3e1870c0
Nami Akazawa <akazawan73@gmail.com>
2019-06-23 11:42:18 +0900

    FIXED

--- HWK4/HWK4_1.py
+++ HWK4/HWK4_1.py
@@ -2,9 +2,6 @@ class Vertex:
     def __init__(self, id):
         self.id = id
         self.neighbors = []
-        self.visited = False
-        self.prev = None
-        self.dist = 0
 
     def addNeighbor(self, neigh):
         self.neighbors.append(neigh)
@@ -26,8 +23,7 @@ class Graph:
 
     def addVertex(self, id):
         self.numVerticies += 1
-        newV = Vertex(id)
-        self.verticies[id] = newV
+        self.verticies[id] = Vertex(id)
 
     def addEdge(self, from_id, to_id):
         if from_id not in self.verticies.keys():
@@ -42,43 +38,43 @@ def bfs(graph, start, goal):
     Return:
         A boolean value True meaning it has a path
     """
-    for vertex in graph.verticies.values():
-        vertex.visited = False
-        vertex.dist = 0
-        vertex.prev = None
+    is_visited = [False] * graph.numVerticies
+    dist = [0] * graph.numVerticies
+    prev = [None] * graph.numVerticies
 
     queue = [start]
-    start.visited = True
+    is_visited[start.id] = True
 
     while queue:
         vertex = queue.pop(0)
         for neighbor in graph.verticies[vertex.id].neighbors:
-            if not neighbor.visited:
-                neighbor.visited = True
-                neighbor.dist = vertex.dist + 1
-                neighbor.prev = vertex
+            if not is_visited[neighbor.id]:
+                id = neighbor.id
+                is_visited[id] = True
+                dist[id] = dist[vertex.id] + 1
+                prev[id] = vertex.id
                 queue.append(neighbor)
                 if neighbor == goal:
-                    return True
-    return False
+                    return prev, dist
+    return None, None
 
 
 def print_bfs(graph, start_id, goal_id, names):
 
     start = graph.verticies[start_id]
     goal = graph.verticies[goal_id]
+    path, dist = bfs(graph, start, goal)
+    if path:
+        temp = goal_id
+        shortest_path = [names[goal_id]]
+        while path[temp]:
+            shortest_path.append(names[path[temp]])
+            temp = path[temp]
 
-    if bfs(graph, start, goal):
-        path = [names[goal.id]]
-        previous = goal
-        while previous.prev:
-            path.append(names[previous.prev.id])
-            previous = previous.prev
+        print(f"{dist[goal_id]} shortest path steps.")
 
-        print(f"Shortest path length is : {goal.dist}")
-
-        print(f"Shortest path is : ")
-        print(path[::-1])
+        print(f"Shortest path is : ", end=" ")
+        print(shortest_path[::-1])
     else:
         print("Oupsy Not connected!!")
 
